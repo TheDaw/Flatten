b3a51d4b9fdafe49977bb29e38fa92ad0b8ecc9c
James Nolan <th3daw@gmail.com>
2016-05-21 22:34:29 +0100

    Flatten an array
    Code + Unit Tests

---
+++ Array_Unit_Tests/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Intercom;
+
+namespace Array_Unit_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void oneList()
+        {
+            var oneList = new List<object> {1 };
+            oneList = Program.Flatten(oneList);
+            Assert.AreEqual(1, oneList[0]);
+        }
+
+        [TestMethod]
+        public void unNested()
+        {
+            var control = new List<object> { 1, 2, 3, 4, 5 };                        
+            var oneList = new List<object> { 1, 2, 3, 4, 5 };
+            oneList = Program.Flatten(oneList);
+            Assert.AreEqual(control[3], oneList[3]);
+        }
+
+        [TestMethod]
+        public void nested()
+        {
+            var control = new List<object> { 1, 2, 3, 4, 5 };
+            var oneList = new List<object> { 1, new List<object> { 2, new List<object> { 3 }, 4 }, 5 };
+            oneList = Program.Flatten(oneList);
+            Assert.AreEqual(control[4], oneList[4]);
+        }
+    }
+}
---
+++ Intercom/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intercom
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = createArrayForTest();
+
+            var test = new List<object>();
+            test = Flatten(list);
+
+            outputList(test);
+
+            Console.ReadLine();
+        }
+
+        private static List<object> createArrayForTest()
+        {
+            var list = new List<object>();
+
+            list.Add(new List<object> { 1 });
+            list.Add(2);
+            list.Add(new List<object> { new List<object> { 3, new List<object> { 1, 2, 3, 4, 5, 6 } , 4 }, 5 });
+            list.Add(new List<object> { new List<object> { new List<object> {5256,6549684,121 },351651 } ,1,1,10,002,64});
+            list.Add(new List<object> { new List<object> { new List<object> { 6 } ,6666} ,1234});
+            list.Add(7);
+            list.Add(8);
+            list.Add(new List<object> { });
+
+            return list;
+        }
+
+        private static void outputList(List<object> list)
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static List<object> Flatten(List<object> list)
+        {
+            var result = new List<object>();
+            foreach (var item in list)
+            {
+                if (item is List<object>)
+                {
+                    result.AddRange(Flatten(item as List<object>));
+                }
+                else
+                {
+                    result.Add(item);                    
+                }
+            }
+            return result;
+        }
+    }
+}
